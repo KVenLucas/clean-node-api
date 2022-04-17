6e432a8c0cfbd6cc80f6867e4a37b49937dcb9d2
KvenLucas <k_l.s@outlook.com>
2022-04-17 12:00:40 -0300

    refactor: simplify imports

--- src/presentation/helpers/http.helper.ts
+++ src/presentation/helpers/http.helper.ts
@@ -1,4 +1,4 @@
-import { HttpResponse } from '../protocols/http'
+import { HttpResponse } from '../protocols'
 import { ServerError as ServerErrorInternal } from '../errors'
 
 export const BadRequest = (error: Error): HttpResponse => ({
